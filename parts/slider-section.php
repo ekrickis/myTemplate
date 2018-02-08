8f078489e8a165df7b8de258a41ee984c1333495
Edvards Krickis <edvards.krickis@kebbeit.lv>
2018-02-08 14:40:36 +0200

    Slider section

---
+++ parts/slider-section.php
@@ -0,0 +1,25 @@
+<div class="slider-section">
+	<div class="slider-section__slider center">
+			<div class="slider__slide">
+				<div class="slider__slide-image">
+					<img src="<?php echo get_image ('slider');?>" alt="">
+					<a hred="#" class="slide-image__title">						
+						Shop Lyle & Scott
+						<i class="fa fa-angle-right"></i>
+					</a>
+				</div>
+			</div>		
+
+			<div class="slider__slide">
+				<div class="slider__slide-image">
+					<img src="<?php echo get_image ('slider');?>" alt="">
+				</div>
+			</div>	
+
+			<div class="slider__slide">
+				<div class="slider__slide-image">
+					<img src="<?php echo get_image ('slider');?>" alt="">
+				</div>
+			</div>
+	</div>
+</div>
